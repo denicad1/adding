4c6243628ce2eabc5b12c4124449a98ea1d1e638
Anthony Denicolo <denicoloa@live.bucks.edu>
2018-06-27 14:07:04 -0400

    //

---
+++ adding/js/main.js
@@ -0,0 +1,13 @@
+﻿//will add every number from 0 to the argument intger
+function addingNum(num) {
+    //variable to hold the total number
+    var tempnum = 0;
+    //loop to include every number from 0 to the argument integer
+    for (var i = 0; i <= num; i++) {
+        //adds the value of i to tempnum
+        tempnum += i;
+    }
+    //logs variable tempnum
+    console.log(tempnum);
+};
+addingNum(12);
